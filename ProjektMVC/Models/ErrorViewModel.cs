4e8326fa7c3f7301911bcd5b3eb7fe69c971e4d6
SylwekD <rutki2268@gmail.com>
2024-10-15 13:19:16 +0200

    Dodaj pliki projektów.

---
+++ ProjektMVC/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ProjektMVC.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
